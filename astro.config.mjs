b0e92fcb10051f7d43aacae81c0c3046f555b3dc
Javier Galera <gr8393@gmail.com>
2024-11-16 16:17:59 +0100

    añadir configuración de vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config';
 import dotenv from 'dotenv';
 
 // Cargar las variables de entorno desde el archivo .env
+import vercel from "@astrojs/vercel/serverless";
 dotenv.config();
+
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
+  output: 'server',
+  adapter: vercel()
 });
