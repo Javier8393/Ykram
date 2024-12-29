9ece6788b9b81d503ff354047d6262722c5f473e
Javier Galera <gr8393@gmail.com>
2024-12-29 12:12:50 +0100

    api env actualizado

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import dotenv from 'dotenv';
 
 // Cargar las variables de entorno desde el archivo .env
 import vercel from "@astrojs/vercel/serverless";
+import mdx from '@astrojs/mdx';
 dotenv.config();
 
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [mdx()]
 });
